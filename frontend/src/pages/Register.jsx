2ad01dc57c964c024936904f29cae6df3c87a513
layuntaFelipe <layunta.felipe@gmail.com>
2022-07-29 17:58:32 -0300

    frontend-backend-register-done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {toast} from 'react-toastify';
 import {FaUser} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {register, reset} from '../features/auth/authSlice';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,6 +13,23 @@ const Register = () => {
     password2: ''
   });
   const {name, email, password, password2} = formData;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {user, isLoading, isError , isSuccess, message} = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    // Redireect when logged in
+    if(isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -23,6 +43,14 @@ const Register = () => {
 
     if(password !== password2) {
       toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+
+      dispatch(register(userData))
     }
   }
 
