93b0a4c84f23932b316498fad45ea8553ff6a33c
layuntaFelipe <layunta.felipe@gmail.com>
2022-07-29 18:08:30 -0300

    logout-user-done

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -27,6 +27,11 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
  console.log(user)
 })
 
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+ await authService.logout();
+})
+
 export const authSlice = createSlice({
  name: 'auth',
  initialState,
@@ -51,6 +56,9 @@ export const authSlice = createSlice({
    state.isLoading = false
    state.isError = true
    state.message = action.payload
+   state.user = null
+  })
+  .addCase(logout.fulfilled, (state) => {
    state.user = null
   })
  }
