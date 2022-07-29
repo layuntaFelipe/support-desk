8724c98b6a991870019d249b0fd4494e51b46be3
layuntaFelipe <layunta.felipe@gmail.com>
2022-07-29 00:56:40 -0300

    register-user-to-database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,50 @@
 const { reset } = require("nodemon");
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
 const registerUser = asyncHandler( async (req, res) => {
- const {name, email, password} = req.body;
+  const {name, email, password} = req.body;
 
- //Validation
- if(!name || !email || !password) {
-  res.status(400)
-  throw new Error('Please include all fields');
- }
+  //Validation
+  if(!name || !email || !password) {
+   res.status(400)
+   throw new Error('Please include all fields');
+  }
 
- res.send('Register Route');
+  // Find if user already exists
+  const userExists = await User.findOne({email});
+
+  if(userExists) {
+   res.status(400)
+   throw new Error('User alredy exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassoword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+   name,
+   email,
+   password: hashedPassoword
+  })
+
+  if(user) {
+   res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email
+   })
+  } else {
+   res.status(400)
+   throw new Error('Invalid user data');
+  }
 });
 
 // @desc    Login a useer
