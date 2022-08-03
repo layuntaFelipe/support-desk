e2c778fedb39698565a9bc6c2e6fd34d7f42fc9e
layuntaFelipe <layunta.felipe@gmail.com>
2022-08-03 19:02:31 -0300

    notes-backend-created

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose');
 
 const ticketSchema = mongoose.Schema({
   user: {
-   type: mongoose.Schema.Types.ObjectId,
-   require: true,
-   ref: 'User'
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: 'User'
   },
   product: {
-   type: String,
-   require: [true, "Please select a product"],
-   enum: ['IPhone', 'Macbook Pro', 'IMac', 'Macbook Air', 'IPad', 'Airpods', 'IPod'],
+    type: String,
+    require: [true, "Please select a product"],
+    enum: ['IPhone', 'Macbook Pro', 'IMac', 'Macbook Air', 'IPad', 'Airpods', 'IPod'],
   },
   description: {
-   type: String,
-   require: [true, 'Please enter a description of the issue']
+    type: String,
+    require: [true, 'Please enter a description of the issue']
   },
   status: {
-   type: String,
-   require: true,
-   enum: ['new', 'open', 'closed'],
-   default: 'new'
+    type: String,
+    require: true,
+    enum: ['new', 'open', 'closed'],
+    default: 'new'
   }
- },
- {
-  timestamps: true,
- }
+  },
+  {
+    timestamps: true,
+  }
 )
 
 module.exports = mongoose.model('Ticket', ticketSchema);
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,6 +4,10 @@ const {getTickets, getTicket, createTickets, deleteTicket, updateTicket} = requi
 
 const {protect} = require('../middleware/authMiddleware');
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes', noteRouter);
+
 
 router.route('/').get(protect, getTickets).post(protect, createTickets);
 router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket);
