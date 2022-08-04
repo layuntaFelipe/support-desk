493db65107d0ce2d9416032129f324057b57e878
layuntaFelipe <layunta.felipe@gmail.com>
2022-08-04 18:51:40 -0300

    ready-for-deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -21,6 +22,20 @@ app.get(`/`, (req, res) => {
 //Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req,res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+  app.get(`/`, (req, res) => {
+   res.json({
+    message: 'Welcome to thee Support Desk API'
+   })
+  })
+}
 
 app.use(errorHandler);
 
